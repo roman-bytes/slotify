c3792fef97841cf5a74ab0d9960b49ea5f0e9951
Jacob Roman <jcbrmn06@gmail.com>
2020-02-03 16:28:24 -0500

    starts setting up album pages

--- www/slotify/album.php
+++ www/slotify/album.php
@@ -1,3 +1,21 @@
-<?php include("includes/header.php"); ?>
+<?php include("includes/header.php"); 
+
+  if (isset($_GET['id'])) {
+    $albumId = $_GET['id'];
+  } else {
+    header("Location: index.php");
+  }
+
+  $albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE id='$albumId'");
+  $album = mysqli_fetch_array($albumQuery);
+
+  $album = new Album($con, $albumId);
+  $artist = $album->getArtist();
+
+
+?>
+<div class="app-content">
+
+</div>
 
 <?php include("includes/footer.php"); ?>
